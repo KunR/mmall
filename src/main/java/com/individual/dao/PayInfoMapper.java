05cb68b6543df679931ebef2227586ae3950aaf8
Yangzubin <yangzubin11@163.com>
2018-08-12 10:38:43 +0800

    映射文件生成

---
+++ src/main/java/com/individual/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.individual.dao;
+
+import com.individual.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
